cf4260bbd788cb10e5e4f2104d0b9d9fe06df312
alireza-sharifpour <ascode94@gmail.com>
2024-04-01 20:25:48 +0330

    completed the function

--- scripts/DeployWithViem.ts
+++ scripts/DeployWithViem.ts
@@ -2,104 +2,193 @@ import {
   createPublicClient,
   http,
   createWalletClient,
+  parseEther,
   formatEther,
   toHex,
-  Address,
+  hexToString,
 } from "viem";
-import { privateKeyToAccount } from "viem/accounts";
 import { sepolia } from "viem/chains";
+import { privateKeyToAccount } from "viem/accounts";
 import * as dotenv from "dotenv";
-import {
-  abi as abiTokenizedBallot,
-  bytecode as bytecodeTokenizedBallot,
-} from "../artifacts/contracts/TokenizedBallot.sol/TokenizedBallot.json";
-import {
-  abi as abiMyToken,
-  bytecode as bytecodeMyToken,
-} from "../artifacts/contracts/MyToken.sol/MyToken.json"; // Add this line
-
+import myToken from "../artifacts/contracts/MyToken.sol/MyToken.json";
+import tokenizedBallot from "../artifacts/contracts/TokenizedBallot.sol/TokenizedBallot.json";
 dotenv.config();
 
-const providerApiKey = process.env.ALCHEMY_API_KEY || "";
-const deployerPrivateKey = process.env.PRIVATE_KEY || "";
+// Define deployer and second account private keys
+const deployerPrivateKey: string = process.env.PRIVATE_KEY || "";
+const secondAddressPrivateKey: string = process.env.SECOND_PRIVATE_KEY || "";
 
+// Define the amount to mint for the deployer
+const MINT_VALUE = parseEther("100");
+
+// Main function
 async function main() {
-  const proposals = process.argv.slice(2);
-  if (!proposals || proposals.length < 1)
-    throw new Error("Proposals not provided");
+  // Convert private keys to account objects
+  const deployerAccount = privateKeyToAccount(`0x${deployerPrivateKey}`);
+  const secondAccount = privateKeyToAccount(`0x${secondAddressPrivateKey}`);
 
-  const publicClient = createPublicClient({
+  // Create Viem clients
+  const publicClient = await createPublicClient({
     chain: sepolia,
-    transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
+    transport: http(
+      `https://eth-sepolia.g.alchemy.com/v2/${
+        process.env.ALCHEMY_API_KEY || ""
+      }`
+    ),
   });
-
-  const account = privateKeyToAccount(`0x${deployerPrivateKey}`);
   const deployer = createWalletClient({
-    account,
+    account: deployerAccount,
     chain: sepolia,
-    transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
+    transport: http(
+      `https://eth-sepolia.g.alchemy.com/v2/${
+        process.env.ALCHEMY_API_KEY || ""
+      }`
+    ),
   });
 
-  // Deploy MyToken contract first
-  console.log("\nDeploying MyToken contract");
-  const myTokenDeploymentHash = await deployer.deployContract({
-    abi: abiMyToken,
-    bytecode: bytecodeMyToken as Address,
-    args: [], // No constructor arguments for MyToken
+  // Display deployer's address and balance
+  console.log("Deployer address:", deployer.account.address);
+  const balance = await publicClient.getBalance({
+    address: deployer.account.address,
   });
   console.log(
-    "Transaction hash for MyToken deployment:",
-    myTokenDeploymentHash
+    "Deployer balance:",
+    formatEther(balance),
+    deployer.chain.nativeCurrency.symbol
   );
-  console.log("Waiting for MyToken contract deployment to be confirmed...");
-  const myTokenReceipt = await publicClient.waitForTransactionReceipt({
-    hash: myTokenDeploymentHash,
+
+  // Deploy MyToken contract
+  console.log("\nDeploying MyToken contract");
+  const hash = await deployer.deployContract({
+    abi: myToken.abi,
+    bytecode: myToken.bytecode as `0x${string}`,
   });
-  console.log("MyToken contract deployed to:", myTokenReceipt.contractAddress);
+  console.log("Transaction hash:", hash);
+  console.log("Waiting for confirmations...");
+  const receipt = await publicClient.waitForTransactionReceipt({ hash: hash });
+  const tokenContractAddress = receipt.contractAddress;
+  console.log("Token contract deployed to:", tokenContractAddress);
 
-  // Assuming targetBlockNumber is correctly set or calculated here
-  const targetBlockNumber = await publicClient.getBlockNumber();
+  // Mint tokens to the deployer
+  const mintTx = await deployer.writeContract({
+    address: tokenContractAddress!,
+    abi: myToken.abi,
+    functionName: "mint",
+    args: [deployer.account.address, MINT_VALUE],
+  });
+  await publicClient.waitForTransactionReceipt({ hash: mintTx });
+  console.log("Transaction hash: ", mintTx);
+  const balanceDeployer = await publicClient.readContract({
+    address: tokenContractAddress!,
+    abi: myToken.abi,
+    functionName: "balanceOf",
+    args: [deployer.account.address],
+  });
+  console.log(
+    `Deployer ${deployer.account.address} has ${formatEther(
+      balanceDeployer as bigint
+    )} units of MyToken\n`
+  );
 
-  // Now deploy TokenizedBallot using the address of the deployed MyToken
-  console.log("\nDeploying TokenizedBallot contract");
-  const tokenizedBallotHash = await deployer.deployContract({
-    abi: abiTokenizedBallot,
-    bytecode: bytecodeTokenizedBallot as Address,
+  // Transfer tokens to the second account
+  const trTxSecond = await deployer.writeContract({
+    address: tokenContractAddress!,
+    abi: myToken.abi,
+    functionName: "transfer",
+    args: [secondAccount.address, parseEther("20")],
+  });
+  await publicClient.waitForTransactionReceipt({ hash: trTxSecond });
+  const balanceSecond = await publicClient.readContract({
+    address: tokenContractAddress!,
+    abi: myToken.abi,
+    functionName: "balanceOf",
+    args: [secondAccount.address],
+  });
+  console.log(
+    `Second account ${secondAccount.address} has ${formatEther(
+      balanceSecond as bigint
+    )} units of MyToken after TX\n`
+  );
+
+  // Delegate voting power from deployer to the second account
+  const delgTxSecond = await deployer.writeContract({
+    address: tokenContractAddress!,
+    abi: myToken.abi,
+    functionName: "delegate",
+    args: [secondAccount.address],
+  });
+  await publicClient.waitForTransactionReceipt({ hash: delgTxSecond });
+  const votesSecond = await publicClient.readContract({
+    address: tokenContractAddress!,
+    abi: myToken.abi,
+    functionName: "getVotes",
+    args: [secondAccount.address],
+  });
+  console.log(
+    `Second account ${secondAccount.address} has ${formatEther(
+      votesSecond as bigint
+    )} units of voting power after delegating\n`
+  );
+
+  // Get the current block number
+  const blockNumber = await publicClient.getBlockNumber();
+  console.log("Blocknumber is: ", blockNumber);
+
+  // Define proposals for the Tokenized Ballot contract
+  const PROPOSALS = ["Vanilla", "Chocolate", "Strawberry"];
+
+  // Deploy Tokenized Ballot contract
+  console.log("\nDeploying Tokenized Ballot contract");
+  const hash2 = await deployer.deployContract({
+    abi: tokenizedBallot.abi,
+    bytecode: tokenizedBallot.bytecode as `0x${string}`,
     args: [
-      proposals.map((prop) => toHex(prop, { size: 32 })), // Proposal names
-      myTokenReceipt.contractAddress, // Use MyToken contract address here
-      targetBlockNumber.toString(), // Convert to string if necessary
+      PROPOSALS.map((prop) => toHex(prop, { size: 32 })),
+      tokenContractAddress,
+      blockNumber,
     ],
   });
-  console.log(
-    "Transaction hash for TokenizedBallot deployment:",
-    tokenizedBallotHash
-  );
-  console.log(
-    "Waiting for TokenizedBallot contract deployment to be confirmed..."
-  );
-  const tokenizedBallotReceipt = await publicClient.waitForTransactionReceipt({
-    hash: tokenizedBallotHash,
+  console.log("Transaction hash2:", hash2);
+  console.log("Waiting for confirmations...");
+
+  const receipt2 = await publicClient.waitForTransactionReceipt({
+    hash: hash2,
   });
+  const ballotContractAddress = receipt2.contractAddress;
+  console.log("Ballot contract deployed to:", ballotContractAddress);
+
+  // Cast vote from the second account
+  const voteTxSecond = await deployer.writeContract({
+    address: ballotContractAddress!,
+    abi: tokenizedBallot.abi,
+    functionName: "vote",
+    args: [BigInt(0), parseEther("1")],
+  });
+  await publicClient.waitForTransactionReceipt({ hash: voteTxSecond });
+  console.log("Second account voted");
+
+  // Retrieve winner name and past votes
+  const winnerName = await publicClient.readContract({
+    address: ballotContractAddress!,
+    abi: tokenizedBallot.abi,
+    functionName: "winnerName",
+    args: [],
+  });
+  const blockNumber2 = await publicClient.getBlockNumber();
+  const pastVotes = await publicClient.readContract({
+    address: tokenContractAddress!,
+    abi: myToken.abi,
+    functionName: "getPastVotes",
+    args: [secondAccount.address, blockNumber2 - 1n],
+  });
+  console.log("Past votes of second account: ", pastVotes);
   console.log(
-    "TokenizedBallot contract deployed to:",
-    tokenizedBallotReceipt.contractAddress
+    "The winning proposal is: " +
+      hexToString(winnerName as `0x${string}`, { size: 32 })
   );
 }
 
-main().catch((error) => {
-  console.error(error);
+main().catch((err) => {
+  console.log(err);
   process.exitCode = 1;
 });
-
-// npx ts-node --files ./scripts/DeployWithViem.ts "Proposal1" "Proposal2" "Proposal3"
-
-// Deploying MyToken contract
-// Transaction hash for MyToken deployment: 0x227d6699a74489f465396cb4611be866fc56088c4734ff878ee23bbba92167f7
-// Waiting for MyToken contract deployment to be confirmed...
-// MyToken contract deployed to: 0x3e1c841928ac8136674dbc4006b442c9cf1cb3d8
-
-// Deploying TokenizedBallot contract
-// Transaction hash for TokenizedBallot deployment: 0xdfdaf26d7db408bf7a530eb184163f1598f0535362c1a35822f85d4e42e45526
-// Waiting for TokenizedBallot contract deployment to be confirmed...
-// TokenizedBallot contract deployed to: 0x9e3c14cb2a890b05d2068f2b905ea7af8aae6ff5
